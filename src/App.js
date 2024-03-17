689cd393dd612cde936ff72fa9f442d0f9a15d2e
Alxost <alexander.stommel@gmx.de>
2024-03-17 14:58:43 +0100

    added class PlayerConfig

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
-function App() {
+import React,{Component} from "react"
+class App extends Component {
+  render(){
   return (
     <div className="App">
+      <PlayerConfig/>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/containers/PlayerConfig.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react"
+
+class PlayerConfig extends Component {
+    render(){
+        return(
+            <h1>PlayerConfig</h1>
+        )
+    }
+}
